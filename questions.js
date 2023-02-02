1be9986446b2dfb6622cec6ebc15baf2b9971637
GustavoBryan <devgustavob@gmail.com>
2023-02-02 16:19:40 -0300

    adição de mais styles e itens

---
+++ questions.js
@@ -0,0 +1,41 @@
+export default [
+    {
+        question: "Tenho ultimate global. Quem sou eu?",
+        answers: [
+            { option: "Lux", correct: false },
+            { option: "Senna", correct: false },
+            { option: "Draven", correct: true },
+            { option: "Ashe", correct: false },
+        ],
+    },
+
+    {
+        question: "Posso ser usado em diferentes lanes. Quem sou eu?",
+        answers: [
+            { option: "Lux", correct: false },
+            { option: "Graves", correct: false },
+            { option: "Malphite", correct: true },
+            { option: "Lulu", correct: false },
+        ],
+    },
+
+    {
+        question: "Posso te salvar. Quem sou eu?",
+        answers: [
+            { option: "Zilean", correct: false },
+            { option: "Taric", correct: false },
+            { option: "Soraka", correct: true },
+            { option: "Sona", correct: false },
+        ],
+    },
+
+    {
+        question: "Sirvo tanto como tank ou com velocidade de ataque. Quem sou eu?",
+        answers: [
+            { option: "Darius", correct: false },
+            { option: "Sett", correct: false },
+            { option: "Garen", correct: true },
+            { option: "Fiora", correct: false },
+        ],
+    },
+]
